68d84e26c5bb8dc30852d54d2c8c3e411bc6cd05
pilimai10 <53910783+pilimai10@users.noreply.github.com>
2020-11-15 21:32:11 -0600

    Add files via upload

---
+++ time_test.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+import logging
+from waveshare_epd import epd2in13_V2
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging.info("time test")
+    
+    epd = epd2in13_V2.EPD()
+    logging.info("init and Clear")
+    epd.init(epd.FULL_UPDATE)
+    epd.Clear(0xFF)
+    
+    # Drawing on the image
+    
+    logging.info("Drawing Test")
+    draw.rectangle([(0,0),(50,50)],outline = 0)
+    draw.rectangle([(55,0),(100,50)],fill = 0)
+    draw.line([(0,0),(50,50)], fill = 0,width = 1)
+    draw.line([(0,50),(50,0)], fill = 0,width = 1)
+    draw.chord((10, 60, 50, 100), 0, 360, fill = 0)
+    draw.ellipse((55, 60, 95, 100), outline = 0)
+    draw.pieslice((55, 60, 95, 100), 90, 180, outline = 0)
+    draw.pieslice((55, 60, 95, 100), 270, 360, fill = 0)
+    draw.polygon([(110,0),(110,50),(150,25)],outline = 0)
+    draw.polygon([(190,0),(190,50),(150,25)],fill = 0)
+    epd.display(epd.getbuffer(image))
+    time.sleep(10)
+    
+    epd.Clear(0xFF)
+    
+    # # partial update
+    logging.info("Time")
+    time_image = Image.new('1', (epd.height, epd.width), 255)
+    time_draw = ImageDraw.Draw(time_image)
+    
+    epd.init(epd.FULL_UPDATE)
+    epd.displayPartBaseImage(epd.getbuffer(time_image))
+    
+    epd.init(epd.PART_UPDATE)
+    num = 0
+    while (True):
+        time_draw.rectangle((120, 80, 220, 105), fill = 255)
+        time_draw.text((120, 80), time.strftime('%H:%M:%S'), font = font24, fill = 0)
+        epd.displayPartial(epd.getbuffer(time_image))
+        num = num + 1
+        if(num == 10):
+            break
+    
+    logging.info("Clear...")
+    epd.init(epd.FULL_UPDATE)
+    epd.Clear(0xFF)
+    
+    logging.info("Goto Sleep...")
+    epd.sleep()
+    epd.Dev_exit()
+        
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    epd2in13_V2.epdconfig.module_exit()
+    exit()
